e2b34f362d6daece5ec9113e618c1b1f9d4bbcab
XUPERPLAY <tintxicasa2@gmail.com>
2025-08-16 19:22:37 +0200

    Update proxy.php

--- proxy.php
+++ proxy.php
@@ -5,17 +5,17 @@ if (empty($url)) { http_response_code(400); exit('URL vacía'); }
 
 $ch = curl_init($url);
 curl_setopt_array($ch, [
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_HTTPHEADER => [
-        'Origin: https://jxoxkplay.xyz',
-        'Referer: https://jxoxkplay.xyz/',
-        'User-Agent: Mozilla/5.0 (iPad; CPU OS 13_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148'
-    ],
-    CURLOPT_FOLLOWLOCATION => true,
-    CURLOPT_SSL_VERIFYPEER => false
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_HTTPHEADER => [
+    'Origin: https://thedaddy.top',
+    'Referer: https://thedaddy.top/',
+    'User-Agent: Mozilla/5.0 (iPad; CPU OS 13_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148'
+  ],
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_SSL_VERIFYPEER => false
 ]);
 $stream = curl_exec($ch);
-if (curl_errno($ch)) { http_response_code(500); exit('Error CURL'); }
+if (curl_errno($ch)) { http_response_code(500); exit('Error'); }
 curl_close($ch);
 
 header('Content-Type: application/vnd.apple.mpegurl');
